3d48d86670d6903729cd2f05d3118ab52bc687dd
Nikola Leško <nlesko94@gmaill.com>
2024-07-10 09:09:23 +0200

    Add tailwindcss and basic index page

---
+++ layouts/public.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <slot />
+        </main>
+        <footer>
+            <p>&copy; 2021 My Site</p>
+        </footer>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "Cryptoloom",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  }
 })
