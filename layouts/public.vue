0874ad397d17642e173647e1ac5455e1ee5a031c
Nikola Leško <nlesko94@gmaill.com>
2024-07-20 22:52:32 +0200

    Add tailwindcss, public layout, public navbar

--- layouts/public.vue
+++ layouts/public.vue
@@ -1,18 +1,10 @@
-<template>
-    <div>
-        <header>
-            <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <slot />
-        </main>
-        <footer>
-            <p>&copy; 2021 My Site</p>
-        </footer>
-    </div>
+<template>  
+    <PublicPartialsNavbar />
+    <main class="text-neutral-800">
+      <slot />
+    </main>
 </template>
+
+<script setup>
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-
+    <h1>Home</h1>
 </template>
 
 <script setup>
-    definePageMeta({
-        layout: 'public'
-    });
+definePageMeta({
+    layout: 'public'
+});
 </script>
